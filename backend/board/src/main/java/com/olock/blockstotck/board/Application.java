7a405595015aef156a0104e2a0c62a7c9cb6431c
이원영 <sheltonwon@gmail.com>
2023-09-11 11:55:49 +0900

    feat: gateway 등록 완료

--- backend/board/src/main/java/com/olock/blockstotck/board/Application.java
+++ backend/board/src/main/java/com/olock/blockstotck/board/Application.java
@@ -1,11 +1,11 @@
-package com.olock.blockstock.discovery;
+package com.olock.blockstotck.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
--- backend/gateway/src/main/java/com/olock/blockstock/gateway/Application.java
+++ backend/gateway/src/main/java/com/olock/blockstock/gateway/Application.java
@@ -1,11 +1,9 @@
-package com.olock.blockstock.discovery;
+package com.olock.blockstock.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
