d943e9ea44a09958a2e6aa124c3331987df3de3f
이원영 <sheltonwon@gmail.com>
2023-09-11 13:18:39 +0900

    test: client 연결 테스트 추가 S09P22B210-93

--- backend/member/src/main/java/com/olock/blockstock/member/Application.java
+++ backend/member/src/main/java/com/olock/blockstock/member/Application.java
@@ -1,15 +1,24 @@
 package com.olock.blockstock.member;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/member/info")
+	public String info(@Value("${server.port}") String port) {
+		return "member 서비스의 기본 동작 Port: {" + port + "}";
 	}
 
 }
