f9af2f09e2a27529fca74fb3a12ccd9f910d933c
KodaHye <kodhdh09@gmail.com>
2023-09-12 21:59:52 +0900

    feat: FAST API 데이터베이스 연동 S09P22B210-114

--- backend/tactic/main.py
+++ backend/tactic/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from db.conn import engineconn
+from domain.contest.models.trade_info import TradeInfo
 
 app = FastAPI()
 
+engine = engineconn()
+session = engine.sessionmaker()
 
 @app.get("/")
 async def root():
@@ -11,3 +15,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/contest")
+async def root():
+    # DB연동 테스트
+    example = session.query(TradeInfo).all()
+    return example
