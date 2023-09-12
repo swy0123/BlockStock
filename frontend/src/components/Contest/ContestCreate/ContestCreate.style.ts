4866e233222f1721cc941b6e4c65a55e4f4caac0
kim <rlaalsrbs15@naver.com>
2023-09-13 07:10:34 +0900

    feat: 대회 생성

---
+++ frontend/src/components/Contest/ContestCreate/ContestCreate.style.ts
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: absolute;
+    width: 713.02px;
+    height: 641px;
+    left: 679px;
+    top: 182px;
+
+    background: #FFFFFF;
+    box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.25);
+    border-radius: 13px;
+    z-index: 3;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    margin: 30px 0px 10px 40px;
+`;
+
+export const CreateTitle = styled.div`
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+display: flex;
+align-items: center;
+letter-spacing: 0.01em;
+
+color: #595959;
+`;
+
+export const CreateImage = styled.img`
+width: 35px;
+margin: 0px 0px 0px 430px;
+`;
+
+export const CreateNickName = styled.div`
+margin: 2px 0px 0px 5px;
+display: flex;
+align-items: center;
+color: #595959;
+`;
+
+export const ContestNameBox = styled.div`
+display: flex;
+margin: 20px 0px 0px 80px;
+`;
+
+export const ContestName = styled.div`
+font-family: 'Aksara Bali Galang';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 49px;
+
+color: #4D5055;
+margin: 0px 40px 0px 0px;
+`;
+
+export const ContestNameInput = styled.input`
+box-sizing: border-box;
+background: #FFFFFF;
+border: 3px solid rgba(160, 155, 163, 0.59);
+border-radius: 6px;
+width: 450.62px;
+height: 46.07px;
+margin-top: 3px;
+font-size: 18px;
+`;
+
+export const ContestStock = styled.div`
+display: flex;
+margin: 20px 0px 0px 80px;
+`;
+
+export const StockName = styled.div`
+font-family: 'Aksara Bali Galang';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 49px;
+
+color: #4D5055;
+margin: 0px 40px 0px 0px;
+`;
+
+export const ContestStockInput = styled.input`
+box-sizing: border-box;
+background: #FFFFFF;
+border: 3px solid rgba(160, 155, 163, 0.59);
+border-radius: 6px;
+width: 450.62px;
+height: 46.07px;
+margin-top: 3px;
+font-size: 18px;
+`;
+
+export const ContestPeriod = styled.div`
+`;
+
+export const PeriodName = styled.div`
+`;
+
+export const ContestPeriodbutton = styled.button`
+`;
+
