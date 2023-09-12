ae8e1506ab018419e3d84a95e0808244d31119ac
이시영 <swy05123@gmail.com>
2023-09-12 20:17:21 +0900

    feat: 블록 코드 컴포넌트화

--- frontend/src/components/BlockCoding/OptionLikeListItem.tsx
+++ frontend/src/components/BlockCoding/OptionLikeListItem.tsx
@@ -4,27 +4,22 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 //     {item.optionname}
 //     {item.currate}
 //     {item.cost}
-const OptionLikeListItem = (props:any) => {
+const OptionLikeListItem = (props) => {
     const [optioncode, setOptioncode] = useState(props.optioncode);
     const [optionname, setOtionname] = useState(props.optionname);
     const [currate, setCurrate] = useState(props.currate);
     const [cost, setCost] = useState(props.cost);
-
-    useEffect(() => {
+    
+    useEffect(()=>{
         console.log(props)
-
-    }, [props])
+    },[])
 
     return (
-        <div>
-        sss
+        <div style={{backgroundColor:"yellow", height:"20px"}}>
             {props.optioncode}
-            <div style={{ backgroundColor: "yellow", height: "20px" }}>
-                {props.optioncode}
-                {optionname}
-                {currate}
-                {cost}
-            </div>
+            {optionname}
+            {currate}
+            {cost}
         </div>
 
     )
