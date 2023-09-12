0793695699634ed7dc722e8f23ffef97aba58cf0
이시영 <swy05123@gmail.com>
2023-09-12 17:02:10 +0900

    feat: 블록 코드 컴포넌트화

---
+++ frontend/src/pages/BlockCoding/MakeTactic.tsx
@@ -0,0 +1,80 @@
+
+import React, { ChangeEvent, useState } from 'react';
+import '../../components/Blockly/blocks/customblocks';
+import '../../components/Blockly/generators/generator';
+import CandleChart from '../../components/Chart/CandleChart';
+import styled from 'styled-components';
+import { BlockCodingDiv, HistoryChartDiv, HistorySummary, TacticResult, TacticTitle, TradingHistoryDiv } from './MakeTactic.style';
+import BlockCoding from '../../components/BlockCoding/BlockCoding';
+
+const TestDiv = styled.div`
+    background-color: rgba(255,0,0,0.3);
+    padding: 1px;
+`;
+
+function MakeTactic() {
+    const [flag, setFlag] = useState(true);
+    const [isSearch, setSearch] = useState(true);
+
+    const [title, setTitle] = useState("제목 없는 전략");
+    const MAX_LENGTH = 30;
+
+    const toggleFlag = () => {
+        setFlag(!flag);
+    }
+
+    const handleTitleField = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value.length > MAX_LENGTH) {
+            e.target.value = e.target.value.slice(0, MAX_LENGTH);
+        }
+        setTitle(e.target.value);
+    };
+
+    const setSearchTrue = () => {
+        setSearch(true);
+    }
+
+    const setSearchFasle = () => {
+        setSearch(false);
+    }
+
+
+    return (
+
+        <div>
+            <button onClick={toggleFlag} style={{ height: "40px", width: "100px" }} >toggle</button>
+            {
+                flag ?
+                    <>
+                        <BlockCoding></BlockCoding>
+                    </> :
+                    <>
+                        <TacticTitle></TacticTitle>
+                        <TacticResult>
+                            {/* 매매내역 */}
+                            <TradingHistoryDiv>
+
+                            </TradingHistoryDiv>
+                            <HistoryChartDiv>
+                                {/* 차트 */}
+                                <CandleChart></CandleChart>
+                            </HistoryChartDiv>
+                            <HistorySummary>
+                                {/* 요약 */}
+                                {/* 버튼 */}
+                                <button></button>
+
+                            </HistorySummary>
+                        </TacticResult>
+                    </>
+
+            }
+
+
+        </div>
+
+    )
+
+}
+
+export default MakeTactic
