3ad7c3dd287311f84fa8b62bd16ee0fd70736a97
김민균 <rlaalsrbs15@naver.com>
2023-09-13 12:58:19 +0900

    feat: 대회생성

--- frontend/src/components/Contest/ContestCreate/ContestCreate.tsx
+++ frontend/src/components/Contest/ContestCreate/ContestCreate.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
 import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
-
-// import dayjs, { Dayjs } from "dayjs";
-// import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import DatePicker from 'react-datepicker';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import './style.css'
+import { useNavigate } from "react-router-dom";
 
 import {
     Container,
@@ -21,14 +21,82 @@ import {
     ContestStockInput,
     ContestPeriod,
     PeriodName,
-    ContestPeriodbutton,
+    ContestEtc,
+    ContestTicket,
+    ContestContentBox,
+    ContentName,
+    ContestContent,
+    BtnBox,
+    CreateBtn,
+    CancelBtn
 } from './ContestCreate.style'
 
 function ContestCreate(){
 
-    // const [value, setValue] = React.useState<Dayjs | null>(
-    //     dayjs("2022-04-17T15:30")
-    //   );
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('')
+    const [stockName, setStockName] = useState('')
+    const [ticket, setTicket] = useState(0)
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [activeButton, setActiveButton] = useState('one');
+    const [content, setContent] = useState('')
+
+    const handleButtonClick = (key) => {
+        setActiveButton(key);
+    };
+
+    const buttons = [
+        <div className="btns">
+            <button
+                className={`btn ${activeButton === 'one' ? 'active' : ''}`}
+                onClick={() => handleButtonClick('one')}
+            >
+                1분
+            </button>
+            <button
+                className={`btn ${activeButton === 'two' ? 'active' : ''}`}
+                onClick={() => handleButtonClick('two')}
+            >
+                10분
+            </button>
+            <button
+                className={`btn ${activeButton === 'three' ? 'active' : ''}`}
+                onClick={() => handleButtonClick('three')}
+            >
+                1시간
+            </button>
+            <button
+                className={`btn ${activeButton === 'four' ? 'active' : ''}`}
+                onClick={() => handleButtonClick('four')}
+            >
+                1일
+            </button>
+        </div>
+    ]
+    
+    const handleChange =()=>{
+        const startformattedDate = `${startDate.getFullYear()}.${(startDate.getMonth() + 1)
+            .toString()
+            .padStart(2, "0")}.${startDate.getDate().toString().padStart(2, "0")} ${
+                startDate.getHours().toString().padStart(2, "0")
+          }:${startDate.getMinutes().toString().padStart(2, "0")}:${startDate.getSeconds().toString().padStart(2, "0")}`;
+        const endformattedDate = `${endDate.getFullYear()}.${(endDate.getMonth() + 1)
+            .toString()
+            .padStart(2, "0")}.${endDate.getDate().toString().padStart(2, "0")} ${
+                endDate.getHours().toString().padStart(2, "0")
+          }:${endDate.getMinutes().toString().padStart(2, "0")}:${endDate.getSeconds().toString().padStart(2, "0")}`;
+        console.log(`
+        title ${title} 
+        stockName ${stockName} 
+        ticket ${ticket} 
+        startDate ${startformattedDate} 
+        endDate ${endformattedDate}
+        activeButton ${activeButton}
+        content ${content}
+        `)
+        console.log('대회생성')
+    }
 
     return(
         <Container>
@@ -43,50 +111,91 @@ function ContestCreate(){
 
             <ContestNameBox>
                 <ContestName>대회명</ContestName>
-                <ContestNameInput/>
+                <ContestNameInput 
+                name= 'title' 
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+                />
             </ContestNameBox>
 
             <ContestStock>
                 <StockName>종목명</StockName>
-                <ContestStockInput/>
+                <ContestStockInput
+                name= 'stockname' 
+                value={stockName}
+                onChange={(e)=>setStockName(e.target.value)}
+                />
             </ContestStock>
 
-            <ContestPeriod>
-                <PeriodName></PeriodName>
+            <ContestPeriod style={{display:'flex'}}>
+                <PeriodName>대회 일정</PeriodName>
+                <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    timeInputLabel="Time:"
+                    dateFormat="MM/dd/yyyy h:mm aa"
+                    showTimeInput
+                    className="custom-datepicker"
+                />
 
-                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DemoContainer components={["DateTimePicker", "DateTimePicker"]}>
-                        <DateTimePicker
-                        label="Uncontrolled picker"
-                        defaultValue={dayjs("2022-04-17T15:30")}
-                        style={{ color: "red" }}
-                        />
-                        <DateTimePicker
-                        label="Controlled picker"
-                        value={value}
-                        onChange={(newValue) => setValue(newValue)}
-                        />
-                    </DemoContainer>
-                </LocalizationProvider> */}
-               
-                
+                <div style={{margin:'13px 15px 0px 15px', fontSize: '30px', fontWeight:'bold'}}>~</div>
+
+                <DatePicker
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    timeInputLabel="Time:"
+                    dateFormat="MM/dd/yyyy h:mm aa"
+                    showTimeInput
+                    className="custom-datepicker"
+                />
             </ContestPeriod>
+
+            <ContestEtc>
+                <ContestTicket 
+                type="number" 
+                min="0" 
+                placeholder="티켓 개수"
+                name= 'ticket ' 
+                value={ticket}
+                onChange={(e)=>
+                    setTicket(e.target.value)
+                }
+                />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        '& > *': {
+                        m: 1,
+                        },
+                    }}
+                    >
+                    <ButtonGroup color="secondary" aria-label="medium secondary button group">
+                        {buttons}
+                    </ButtonGroup>
+                </Box>
+            </ContestEtc>
+            
+            <ContestContentBox>
+                <ContentName>상세 내용</ContentName>
+                <ContestContent
+                name= 'content ' 
+                value={content}
+                onChange={(e)=>
+                    setContent(e.target.value)
+                }
+                />
+            </ContestContentBox>
+            <hr style={{margin:'30px 0px 0px 30px', width:'650px', background:'##D3D3D3'}}/>
+            
+            <BtnBox>
+                <CreateBtn onClick={handleChange}>생성</CreateBtn>
+                <CancelBtn onClick={()=>navigate('../')}>취소</CancelBtn>
+            </BtnBox>
 
         </Container>
     )
 }
 
 export default ContestCreate
-
-// () => {
-//     const [startDate, setStartDate] = useState(new Date());
-//     return (
-//       <DatePicker
-//         selected={startDate}
-//         onChange={(date) => setStartDate(date)}
-//         timeInputLabel="Time:"
-//         dateFormat="MM/dd/yyyy h:mm aa"
-//         showTimeInput
-//       />
-//     );
-//   };
