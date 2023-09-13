42a36e89d6df77dcc510e46fc841374e33e7f06d
KodaHye <kodhdh09@gmail.com>
2023-09-13 23:55:27 +0900

    feat: FastAPI Eureka 연동 S09P22B210-114

--- backend/tactic/main.py
+++ backend/tactic/main.py
@@ -3,10 +3,15 @@ from db.conn import engineconn
 from domain.contest.models.trade_info import TradeInfo
 from domain.tactic.routers import tactic
 from domain.contest.routers import contest
+from domain.option.routers import option
 
 import py_eureka_client.eureka_client as eureka_client
 
 app = FastAPI()
+
+app.include_router(tactic.app)
+app.include_router(contest.router)
+app.include_router(option.router)
 
 your_rest_server_port = 64414
 
@@ -19,8 +24,7 @@ async def startup_event():
 engine = engineconn()
 session = engine.sessionmaker()
 
-app.include_router(tactic.app)
-app.include_router(contest.router)
+
 
 @app.get("/")
 async def root():
@@ -29,5 +33,6 @@ async def root():
 @app.get("/contest")
 async def root():
     # DB연동 테스트
-    example = session.query(TradeInfo).all()
-    return example
+    # example = session.query(TradeInfo).all()
+    # return example
+    return {"message": "DB연동 테스트"}
