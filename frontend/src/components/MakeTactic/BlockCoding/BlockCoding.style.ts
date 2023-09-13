de0729b6ceadbcb0b71ce5e227fb8858f06df234
이시영 <swy05123@gmail.com>
2023-09-13 23:54:26 +0900

    feat: 블록 코딩 화면 수정

--- frontend/src/components/MakeTactic/BlockCoding/BlockCoding.style.ts
+++ frontend/src/components/MakeTactic/BlockCoding/BlockCoding.style.ts
@@ -7,6 +7,30 @@ export const BlockCodingContainer = styled.div`
   height: 100%;
   overflow: hidden;
 `;
+export const TitleDiv = styled.div`
+  display: flex;
+  width: 100%;
+  height: 15%;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+export const Title = styled.div`
+  display: flex;
+  width: 90%;
+  height: 100%;
+  align-items: center;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+export const TitleInput = styled.input`
+  width: 90%;
+  height: 80%;
+  /* border: none; */
+  border-radius: 5px;
+  background-color: transparent;
+`;
 export const LeftDiv = styled.div`
   background-color: rgba(0, 0, 0, 0.08);
   display: flex;
@@ -33,7 +57,7 @@ export const IsSearchDiv = styled.div`
   margin-bottom: 50px;
   border-radius: 10px;
   width: 100%;
-  height: 90%;
+  height: 85%;
 `;
 export const SearchType = styled.div<{ $isChecked: boolean }>`
   float: left;
@@ -93,7 +117,7 @@ export const Test = styled.div`
   position: relative;
   width: 100%;
   height: 75%;
-  margin-bottom: 5%;
+  margin-bottom: 1%;
 `;
 export const InputDetailDiv = styled.div`
   position: relative;
@@ -102,12 +126,16 @@ export const InputDetailDiv = styled.div`
   width: 100%;
   height: 5%;
   text-align: center;
+  margin-bottom: 1%;
   /* margin-top: 1%; */
 `;
 export const InputDetailTitle = styled.div`
   position: relative;
   width: 25%;
   height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const InputDetailValue = styled.div`
   position: relative;
@@ -118,8 +146,9 @@ export const InputOptionDiv = styled.div`
   position: relative;
   display: flex;
   flex-direction: row-reverse;
+  align-items: center;
   width: 100%;
-  height: 5%;
-  margin-top: 1%;
+  height: 14%;
+  margin-bottom: 1%;
   align-items: center;
 `;
