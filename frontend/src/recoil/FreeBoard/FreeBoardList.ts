02db9b6582bb56ad49953989691e634c47383c6d
김민균 <rlaalsrbs15@naver.com>
2023-09-15 00:06:47 +0900

    feat: freeboard S09P22B210-156

---
+++ frontend/src/recoil/FreeBoard/FreeBoardList.ts
@@ -0,0 +1,277 @@
+import { atom } from 'recoil';
+
+export const freeBoardList = atom({
+  key: 'freeBoardList',
+  default: [
+    {
+      freeboard: {
+        id: 0,
+        title: "프리보드 글 제목 1",
+        nickname: "User 1",
+        hit: 100,
+        modifiedAt: "2023.09.14 10:30:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 1,
+        title: "프리보드 글 제목 2",
+        nickname: "User 2",
+        hit: 85,
+        modifiedAt: "2023.09.14 09:45:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 2,
+        title: "프리보드 글 제목 3",
+        nickname: "User 3",
+        hit: 120,
+        modifiedAt: "2023.09.13 15:20:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 3,
+        title: "프리보드 글 제목 4",
+        nickname: "User 4",
+        hit: 75,
+        modifiedAt: "2023.09.13 12:15:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 4,
+        title: "프리보드 글 제목 5",
+        nickname: "User 5",
+        hit: 60,
+        modifiedAt: "2023.09.12 14:10:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 5,
+        title: "프리보드 글 제목 6",
+        nickname: "User 6",
+        hit: 95,
+        modifiedAt: "2023.09.12 11:25:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 6,
+        title: "프리보드 글 제목 7",
+        nickname: "User 7",
+        hit: 110,
+        modifiedAt: "2023.09.11 08:50:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 7,
+        title: "프리보드 글 제목 8",
+        nickname: "User 8",
+        hit: 45,
+        modifiedAt: "2023.09.11 06:30:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 8,
+        title: "프리보드 글 제목 9",
+        nickname: "User 9",
+        hit: 80,
+        modifiedAt: "2023.09.10 17:15:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 9,
+        title: "프리보드 글 제목 10",
+        nickname: "User 10",
+        hit: 70,
+        modifiedAt: "2023.09.10 13:55:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 0,
+        title: "프리보드 글 제목 1",
+        nickname: "User 1",
+        hit: 100,
+        modifiedAt: "2023.09.14 10:30:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 1,
+        title: "프리보드 글 제목 2",
+        nickname: "User 2",
+        hit: 85,
+        modifiedAt: "2023.09.14 09:45:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 2,
+        title: "프리보드 글 제목 3",
+        nickname: "User 3",
+        hit: 120,
+        modifiedAt: "2023.09.13 15:20:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 3,
+        title: "프리보드 글 제목 4",
+        nickname: "User 4",
+        hit: 75,
+        modifiedAt: "2023.09.13 12:15:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 4,
+        title: "프리보드 글 제목 5",
+        nickname: "User 5",
+        hit: 60,
+        modifiedAt: "2023.09.12 14:10:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 5,
+        title: "프리보드 글 제목 6",
+        nickname: "User 6",
+        hit: 95,
+        modifiedAt: "2023.09.12 11:25:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 6,
+        title: "프리보드 글 제목 7",
+        nickname: "User 7",
+        hit: 110,
+        modifiedAt: "2023.09.11 08:50:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 7,
+        title: "프리보드 글 제목 8",
+        nickname: "User 8",
+        hit: 45,
+        modifiedAt: "2023.09.11 06:30:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 8,
+        title: "프리보드 글 제목 9",
+        nickname: "User 9",
+        hit: 80,
+        modifiedAt: "2023.09.10 17:15:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 9,
+        title: "프리보드 글 제목 10",
+        nickname: "User 10",
+        hit: 70,
+        modifiedAt: "2023.09.10 13:55:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 0,
+        title: "프리보드 글 제목 1",
+        nickname: "User 1",
+        hit: 100,
+        modifiedAt: "2023.09.14 10:30:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 1,
+        title: "프리보드 글 제목 2",
+        nickname: "User 2",
+        hit: 85,
+        modifiedAt: "2023.09.14 09:45:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 2,
+        title: "프리보드 글 제목 3",
+        nickname: "User 3",
+        hit: 120,
+        modifiedAt: "2023.09.13 15:20:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 3,
+        title: "프리보드 글 제목 4",
+        nickname: "User 4",
+        hit: 75,
+        modifiedAt: "2023.09.13 12:15:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 4,
+        title: "프리보드 글 제목 5",
+        nickname: "User 5",
+        hit: 60,
+        modifiedAt: "2023.09.12 14:10:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 5,
+        title: "프리보드 글 제목 6",
+        nickname: "User 6",
+        hit: 95,
+        modifiedAt: "2023.09.12 11:25:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 6,
+        title: "프리보드 글 제목 7",
+        nickname: "User 7",
+        hit: 110,
+        modifiedAt: "2023.09.11 08:50:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 7,
+        title: "프리보드 글 제목 8",
+        nickname: "User 8",
+        hit: 45,
+        modifiedAt: "2023.09.11 06:30:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 8,
+        title: "프리보드 글 제목 9",
+        nickname: "User 9",
+        hit: 80,
+        modifiedAt: "2023.09.10 17:15:00"
+      }
+    },
+    {
+      freeboard: {
+        id: 9,
+        title: "프리보드 글 제목 10",
+        nickname: "User 10",
+        hit: 70,
+        modifiedAt: "2023.09.10 13:55:00"
+      }
+    }
+  ]
+})
