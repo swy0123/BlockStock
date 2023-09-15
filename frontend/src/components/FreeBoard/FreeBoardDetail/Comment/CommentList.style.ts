a710bd69a030dd4468d6521750ec1c51b332f9f0
김민균 <rlaalsrbs15@naver.com>
2023-09-15 11:13:43 +0900

    feat: 상세페이지

---
+++ frontend/src/components/FreeBoard/FreeBoardDetail/Comment/CommentList.style.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+min-height: 500px;
+`;
+
+export const Header = styled.div`
+display: flex;
+margin: 30px 0px 10px 50px;
+color: #B8B6B6;
+font-size: 18px;
+`;
+export const UserImg = styled.img`
+width: 30px;
+`;
+
+export const NickName = styled.div`
+margin: 2px 10px 0px 20px;
+`;
+export const Day = styled.div`
+margin: 2px 0px 0px 10px;
+`;
+export const DeleteBtn = styled.div`
+margin: 5px 0px 0px 20px;
+width: 60px;
+height: 25px;
+color: white;
+font-size: 14px;
+background: #EC4275;
+border-radius: 6px;
+display: flex;
+justify-content: center; /* 수평 가운데 정렬 */
+align-items: center;
+`;
+export const Comment = styled.div`
+margin: 20px 0px 50px 50px;
+font-size: 20px;
+`;
+
--- frontend/src/components/FreeBoard/FreeBoardDetail/FreeBoardItemDetail.style.ts
+++ frontend/src/components/FreeBoard/FreeBoardDetail/FreeBoardItemDetail.style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-max-width: 1300px;
+width: 1300px;
 min-height: 1500px;
 background: #FFFFFF;
 box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
@@ -10,13 +10,12 @@ margin: 50px 0px 100px 150px;
 `;
 
 export const PostTitle = styled.div`
-font-weight: 400;
-font-size: 32px;
+font-weight: 500;
+font-size: 35px;
 align-items: flex-end;
-padding: 40px 0px 0px 60px;
+padding: 40px 0px 5px 60px;
 color: #000000;
 `;
-
 
 export const Header = styled.div`
 display: flex;
@@ -36,15 +35,17 @@ font-size: 20px;
 margin: 0px 0px 0px 10px;
 `;
 export const Box = styled.div`
-margin: 0px 0px 0px 600px;
+margin: 0px 0px 0px 490px;
 display: flex;
 `;
 export const Hit = styled.div`
 color: #B8B6B6;
 font-size: 20px;
 margin: 0px 0px 0px 10px;
+display: flex;
 `;
 export const Like = styled.div`
+display: flex;
 color: #B8B6B6;
 font-size: 20px;
 margin: 0px 0px 0px 10px;
@@ -53,16 +54,86 @@ export const Comment = styled.div`
 color: #B8B6B6;
 font-size: 20px;
 margin: 0px 0px 0px 10px;
+display: flex;
+`;
+export const Line = styled.div`
+border: 1px solid #F4F1F1;
+width: 100%;
+margin: 30px 0px 0px 0px;
 `;
 export const Wrapper = styled.div`
+width: 100%;
+min-height: 700px;
+/* border: 1px solid black; */
+`;
+export const ContentBox = styled.div`
+width: 90%;
+min-height: 500px;
+padding: 60px;
 `;
 export const Content = styled.div`
+font-size: 20px;
+`;
+export const LikeBtnBox = styled.div`
+  display: flex;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+  width: 100%;
+  min-height: 100px;
 `;
 export const LikeBtn = styled.div`
+width: 117px;
+height: 36px;
+background: #FFFFFF;
+border: 1px solid #A7A7A7;
+border-radius: 200px;
+
+font-weight: 600;
+font-size: 14px;
+line-height: 19px;
+display: flex;
+justify-content: center; /* 수평 가운데 정렬 */
+align-items: center;
+color: #6D6D6D;
+
+`;
+export const BtnBox = styled.div`
+width: 100%;
+min-height: 100px;
+display: flex;
 `;
 export const DeleteBtn = styled.div`
+width: 101.05px;
+height: 34px;
+background: #EC4275;
+border-radius: 6px;
+font-weight: 600;
+font-size: 16px;
+display: flex;
+justify-content: center; /* 수평 가운데 정렬 */
+align-items: center;
+color: #FFFFFF;
+margin: 30px 0px 0px 1000px;
+&:hover{
+  background-color: #B61E35;
+}
 `;
 export const UpdateBtn = styled.div`
+width: 101.05px;
+height: 34px;
+background: #097DF3;
+border-radius: 6px;
+font-weight: 600;
+font-size: 16px;
+display: flex;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center;
+color: #FFFFFF;
+margin: 30px 0px 0px 30px;
+&:hover{
+  background-color: #0050A0;
+}
 `;
+
 
 
