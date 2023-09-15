b50d283720b164120dc3b06a3d40aa1ef5c3eacb
김민균 <rlaalsrbs15@naver.com>
2023-09-15 13:03:37 +0900

    feat: 대회페이지 수정

--- frontend/src/components/Contest/Main/RecentContestResults.style.ts
+++ frontend/src/components/Contest/Main/RecentContestResults.style.ts
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 
 export const Container = styled.div`
   height: 400px;
-  margin: 0px 0px 0px 100px;
+  margin: 0px 0px 0px 140px;
 `;
 
 export const ContestTitleWrapper = styled.div`
@@ -17,14 +17,13 @@ export const ContestTitle = styled.div`
 
 
 export const Wrappe = styled.div`
-  width: 400px;
-  height: 320px;
+  width: 440px;
+  height: 265px;
   border: 2px solid #D4D4D4;
   background: #FFFFFF;
   border: 2px solid #D4D4D4;
   box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
   border-radius: 10px;
-  padding: 40px;
 `;
 
 export const ContestLink = styled.div`
@@ -35,21 +34,20 @@ export const ContestLink = styled.div`
 
 export const RankBox = styled.div`
   display: flex;
-  padding-left: 30px;
-  margin-bottom: 30px;
+  max-height: 60px;
+  margin: 15px 0px 15px 70px;
 `;
 
 export const Rank = styled.div`
-font-size: 25px;
-margin: 10px 0px 0px 0px;
+font-size: 20px;
+margin: 20px 0px 0px 0px;
 color: #8A8A8A;
-margin-top: 20px;
 `;
 
 export const RankImage = styled.img`
-width: 60px;
-height: 60px;
-margin: 10px 25px 0px 50px;
+width: 40px;
+height: 40px;
+margin: 13px 25px 0px 30px;
 border-radius: 50px;
 `;
 
@@ -59,11 +57,11 @@ margin-top: 10px;
 `;
 
 export const RankNickName = styled.div`
-font-size: 25px;
+font-size: 20px;
 font-weight: bold;
 `;
 
 export const RankReturn = styled.div`
 display: flex;
-font-size: 20px;
+font-size: 16px;
 `;
