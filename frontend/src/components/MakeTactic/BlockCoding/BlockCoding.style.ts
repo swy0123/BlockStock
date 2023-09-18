8da7b0ac536938d684595b22b5679eaa3033fced
이시영 <swy05123@gmail.com>
2023-09-18 23:44:48 +0900

    feat: 블록코딩 화면 수정 S09P22B210-157

--- frontend/src/components/MakeTactic/BlockCoding/BlockCoding.style.ts
+++ frontend/src/components/MakeTactic/BlockCoding/BlockCoding.style.ts
@@ -10,34 +10,42 @@ export const BlockCodingContainer = styled.div`
 export const TitleDiv = styled.div`
   display: flex;
   width: 100%;
-  height: 15%;
+  height: 50px;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
+  /* overflow: hidden; */
 `;
 export const Title = styled.div`
   display: flex;
+  font-size: 20px;
+  font-weight: bolder;
   width: 90%;
   height: 100%;
   align-items: center;
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
+  border-style: solid;
+  border-width: 0 0 3px;
+  border-color: transparent;
 `;
 export const TitleInput = styled.input`
   width: 90%;
   height: 80%;
-  /* border: none; */
-  border-radius: 5px;
+  font-size: 20px;
+  font-weight: bolder;
+  outline: none;
+  border-style: solid;
+  border-width: 0 0 3px;
   background-color: transparent;
 `;
 export const LeftDiv = styled.div`
-  background-color: rgba(0, 0, 0, 0.08);
+  /* background-color: rgba(0, 0, 0, 0.08); */
   display: flex;
   justify-content: flex-start;
   flex-direction: column;
   position: relative;
-  width: 25%;
+  width: 27%;
   /* margin-right: 2%; */
   padding: 1%;
   height: 100%;
@@ -54,43 +62,60 @@ export const IsSearchDiv = styled.div`
   box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
   border: 1px solid #d4d4d4;
   margin-top: 20px;
+  padding: 0 5%;
   margin-bottom: 50px;
   border-radius: 10px;
-  width: 100%;
+  width: 90%;
   height: 85%;
-`;
-export const SearchType = styled.div<{ $isChecked: boolean }>`
-  float: left;
-  width: 50%;
-  height: 7%;
-  margin-top: 3%;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const SearchTypeDiv = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`;
+export const SearchType = styled.div<{ $isChecked: boolean }>`
+  /* float: left; */
+  width: 45%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
   text-align: center;
   align-items: center;
   color: ${(props) => (props.$isChecked ? " #9155FD" : "black")};
-  /* border-style: solid; */
+  border-style: solid;
+  border-width: 0 0 3px;
+  border-color: ${(props) => (props.$isChecked ? "#9155FD" : "transparent")};
 `;
 
-export const SearchTypeUnderLine = styled.div<{ $isChecked: boolean }>`
-  float: left;
-  width: 70%;
-  height: 2px;
-  display: flex;
-  margin-top: 2%;
-  background-color: ${(props) => (props.$isChecked ? " #9155FD" : "")};
-  /* border-style: solid; */
+export const SearchInputDiv = styled.div`
+  width: 90%;
+  height: 25px;
+  margin: 2%;
+  margin-top: 4%;
+  position: relative;
+`;
+export const SearchImg = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 20px;
 `;
 
 export const SearchInput = styled.input`
-  width: 92%;
+  width: 100%;
   /* padding: 0; */
-  height: 25px;
-  margin: 2%;
+  height: 100%;
   /* position: absolute; */
-  /* border: none; */
-  border-radius: 5px;
-  /* background-color: transparent; */
+  outline: none;
+  border-width: 0 0 1px;
 `;
 export const SearchItemList = styled.div`
   width: 100%;
@@ -107,17 +132,17 @@ export const BlockCodingDiv = styled.div`
   display: flex;
   justify-content: flex-start;
   flex-direction: column;
-  background-color: rgba(0, 0, 0, 0.08);
+  /* background-color: rgba(0, 0, 0, 0.08); */
   position: relative;
   padding: 1%;
-  width: 75%;
+  width: 72%;
   height: 100%;
 `;
 export const Test = styled.div`
   position: relative;
   width: 100%;
   height: 75%;
-  margin-bottom: 1%;
+  margin-bottom: 5%;
 `;
 export const InputDetailDiv = styled.div`
   position: relative;
