512ba330918bb41a3429257209f69aa4db3dc49f
김민균 <rlaalsrbs15@naver.com>
2023-09-18 12:04:03 +0900

    자유게시판 수정

--- frontend/src/pages/FreeBoard/FreeBoardUpdate.tsx
+++ frontend/src/pages/FreeBoard/FreeBoardUpdate.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import FreeBoardUpdateBox from "../../components/FreeBoard/FreeBoardUpdate/FreeBoardUpdateBox";
+import styled from "styled-components";
 function FreeBoardUpdate(){
+
+  const Container = styled.div`
+  margin: 50px 0px 0px 50px;
+`;
   return(
-    <div>
-      생성
-    </div>
+    <Container>
+      <FreeBoardUpdateBox/>
+    </Container>
   )
 }
 
