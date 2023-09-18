6db41766428e420baa3c9fc0eeb60ca3cd8e2b48
김민균 <rlaalsrbs15@naver.com>
2023-09-18 10:42:21 +0900

    feat: 자유게시판 상세페이지

--- frontend/src/components/FreeBoard/FreeBoardDetail/Comment/CommentList.style.ts
+++ frontend/src/components/FreeBoard/FreeBoardDetail/Comment/CommentList.style.ts
@@ -1,17 +1,20 @@
 import styled from "styled-components";
+import { Title } from "./CommentCeate.style";
 
 export const Container = styled.div`
-min-height: 500px;
+min-height: 200px;
+overflow-y: auto;
 `;
 
 export const Header = styled.div`
 display: flex;
 margin: 30px 0px 10px 50px;
 color: #B8B6B6;
-font-size: 18px;
+font-size: 13px;
 `;
 export const UserImg = styled.img`
-width: 30px;
+width: 23px;
+height: 23px;
 `;
 
 export const NickName = styled.div`
@@ -22,18 +25,25 @@ margin: 2px 0px 0px 10px;
 `;
 export const DeleteBtn = styled.div`
 margin: 5px 0px 0px 20px;
-width: 60px;
-height: 25px;
+width: 40px;
+height: 20px;
 color: white;
-font-size: 14px;
+font-size: 10px;
 background: #EC4275;
 border-radius: 6px;
 display: flex;
+cursor: pointer;
 justify-content: center; /* 수평 가운데 정렬 */
 align-items: center;
 `;
 export const Comment = styled.div`
 margin: 20px 0px 35px 50px;
-font-size: 20px;
+font-size: 13px;
 `;
 
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh; 
+`;
