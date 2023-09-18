7d4e48c052332b70843f8047d29bad0f4572f6ac
김민균 <rlaalsrbs15@naver.com>
2023-09-18 10:42:21 +0900

    feat: 자유게시판 상세페이지

--- frontend/src/components/FreeBoard/FreeBoardDetail/Comment/CommentCeate.tsx
+++ frontend/src/components/FreeBoard/FreeBoardDetail/Comment/CommentCeate.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import {
   Container,
@@ -6,11 +7,25 @@ import {
   Title,
   CommentInput,
   CommentBtn,
+  BackBtn
 } from './CommentCeate.style'
+import {
+  LikeBtnBox,
+  LikeBtn, 
+} from '../FreeBoardItemDetail.style'
 function CommentCreate(){
   return(
     <>
       <Container>
+        <LikeBtnBox>
+            <LikeBtn>
+              <FavoriteIcon style={{color:'red', margin:'0px 6px 0px 0px', width:'22px'}}/>
+              좋아요
+            </LikeBtn>
+            <BackBtn>
+              목록으로
+            </BackBtn>
+          </LikeBtnBox>
         <Wrapper>
           <Title>댓글</Title>
           <CommentInput placeholder="댓글을 작성해주세요"/>
