7709862249d0b4a8e7d4413f3c4b3f02a62387e9
이시영 <swy05123@gmail.com>
2023-09-20 17:39:41 +0900

    feat: 블록 테스트 및 차트 화면 수정 S09P22B210-162

--- frontend/src/components/MakeTactic/TacticResult/TacticResult.style.ts
+++ frontend/src/components/MakeTactic/TacticResult/TacticResult.style.ts
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+export const TacticTitle = styled.div`
+  /* background-color: rgba(0, 150, 0, 0.08); */
+  display: flex;
+  width: 100%;
+  height: 5%;
+  padding: 10px 0 0 10px;
+  font-weight: bolder;
+`;
 export const TradingHistoryContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -14,63 +22,151 @@ export const TradingHistoryContents = styled.div`
   height: 95%;
   overflow: hidden;
 `;
-export const TacticTitle = styled.div`
-  background-color: rgba(0, 150, 0, 0.08);
-  display: flex;
-  width: 100%;
-  height: 5%;
-`;
 
-export const TradingHistoryDiv = styled.div`
-  background-color: rgba(0, 0, 150, 0.08);
+export const LeftDiv = styled.div`
+  /* background-color: rgba(0, 0, 150, 0.08); */
   display: flex;
-  justify-content: flex-start;
   flex-direction: column;
   position: relative;
+  padding: 10px;
   width: 22%;
-  /* margin-right: 2%; */
-  padding: 1%;
   height: 100%;
 `;
-export const HistoryChartDiv = styled.div`
+
+export const CenterDiv = styled.div`
+  background-color: rgba(0, 0, 150, 0.08);
   display: flex;
-  justify-content: flex-start;
   flex-direction: column;
-  background-color: rgba(0, 0, 0, 0.08);
   position: relative;
-  padding: 1%;
+  padding: 10px;
   width: 50%;
   height: 100%;
 `;
-export const HistorySummary = styled.div`
+
+export const RightDiv = styled.div`
+  background-color: rgba(0, 0, 150, 0.08);
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
   flex-direction: column;
   position: relative;
   width: 22%;
-  /* margin-right: 2%; */
-  padding: 1%;
+  padding: 10px;
   height: 100%;
 `;
-export const HistorySummaryContents = styled.div`
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  background-color: rgba(150, 0, 0, 0.08);
+
+export const TradingHistoryDiv = styled.div`
+  background-color: white;
+  box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
+  border: 1px solid #d4d4d4;
+  margin-bottom: 50px;
+  border-radius: 10px;
   width: 100%;
-  margin-bottom: 5%;
-  padding: 1%;
-  height: 80%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
 `;
+
+export const TradingHistoryTitle = styled.div`
+  width: 90%;
+  height: 7%;
+  padding: 10px;
+  display: flex;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const OptionHistoryItemList = styled.div`
+  width: 90%;
+  height: 85%;
+  margin-bottom: 2%;
+  display: flex;
+  border: 1px solid #d4d4d4;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const OptionHistoryItemTitle = styled.div`
+  width: 100%;
+  height: 7%;
+  font-size: 10px;
+  margin-bottom: 1%;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  align-items: center;
+  padding: 5px;
+`;
+
+export const HistoryChartDiv = styled.div`
+  background-color: white;
+  box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
+  border: 1px solid #d4d4d4;
+  margin-bottom: 50px;
+  border-radius: 10px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const HistorySummary = styled.div`
+  margin-bottom: 50px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HistorySummaryContents = styled.div`
+  background-color: white;
+  box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
+  border: 1px solid #d4d4d4;
+  border-radius: 10px;
+  width: 100%;
+  height: 85%;
+  margin-bottom: 5%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HistorySummaryContentsResult = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #B38FFF;
+  border-radius: 5px;
+  width: 70%;
+  height: 26%;
+  text-align: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+// export const HistorySummaryContentsTitle = styled.div`
+//   display: flex;
+//   font-weight: bolder;
+//   font-size: 20px;
+//   width: 100%;
+//   height: 10%;
+//   text-align: center;
+//   align-items: center;
+// `;
+export const HistorySummaryContentsItem = styled.div`
+  position: relative;
+  border-radius: 5px;
+  width: 100%;
+  height: 16%;
+`;
+
 export const HistorySaveButton = styled.button`
   display: flex;
-  position: absolute;
+  position: relative;
   width: 80%;
-  /* margin-right: 2%; */
-  /* padding: 1%; */
   height: 10%;
   justify-content: center;
   align-items: center;
-  bottom: 5%;
 `;
