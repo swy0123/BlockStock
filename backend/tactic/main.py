96dbd9817932927d9cda1adbeed1b6c29d9e2de3
KodaHye <kodhdh09@gmail.com>
2023-09-20 17:14:26 +0900

    fix: FAST API CORS 에러 해결

--- backend/tactic/main.py
+++ backend/tactic/main.py
@@ -1,7 +1,7 @@
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
 from fastapi.middleware.cors import CORSMiddleware
 from db.conn import engineconn
 from domain.tactic.routers import tactic
@@ -11,16 +11,27 @@ from redis_config import redis_config
 
 import py_eureka_client.eureka_client as eureka_client
 
-app = FastAPI()
+app = FastAPI(host="0.0.0.0", port=8000)
 
-origin = [
-    "*"
+origins = [
+    "http://localhost:5173/",
+    "http://127.0.0.1:5173",
+    "https://j9b210.p.ssafy.io:8443/"
+    "https://seal-striking-presumably.ngrok-free.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=origins,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"]
 )
+
+
+@app.options("/{path:path}", status_code=status.HTTP_200_OK)
+async def options_handler(path):
+    return {"Allow" : "GET, POST, PUT, DELETE, OPTIONS"}
+
 app.include_router(tactic.app)
 app.include_router(contest.router)
 app.include_router(option.router)
