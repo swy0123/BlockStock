dbc7b38d1034e06c2f72f42f219ae91acee30a4d
이원영 <sheltonwon@gmail.com>
2023-09-20 13:07:14 +0900

    tmp: cnt 임시 변수 추가

--- backend/member/src/main/java/com/olock/blockstock/member/domain/member/dto/response/MemberInfoResponse.java
+++ backend/member/src/main/java/com/olock/blockstock/member/domain/member/dto/response/MemberInfoResponse.java
@@ -2,25 +2,28 @@ package com.olock.blockstock.member.domain.member.dto.response;
 
 import com.olock.blockstock.member.domain.member.persistence.entity.Member;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class MemberInfoResponse {
     private Long id;
     private String email;
     private String nickname;
-    private Integer ticketCount;
+    private Integer ticketCnt;
     private Long money;
+    private int followerCnt = 12;
+    private int followingCnt = 0;
+    private List<String> award = new ArrayList<>(List.of("제1회 우승", "제2회 우승"));
     private LocalDateTime createdAt;
 
     public MemberInfoResponse(Member member) {
         this.id = member.getId();
         this.email = member.getEmail();
         this.nickname = member.getNickname();
-        this.ticketCount = member.getTicketCount();
+        this.ticketCnt = member.getTicketCount();
         this.money = member.getMoney();
         this.createdAt = member.getCreatedAt();
     }
