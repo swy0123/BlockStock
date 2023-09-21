3c91921c47e3364867675af18468bbfe33d2db7f
wldbs8241 <wldbs8241@naver.com>
2023-09-21 12:52:39 +0900

    feat: 로그아웃 구현 리프레시 진행 중S09P22B210-187

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,78 +1,83 @@
-import axios, { AxiosInstance } from 'axios';
-
+import axios, { AxiosInstance } from "axios";
 
 //수정
-const BASE_URL = 'https://j9b210.p.ssafy.io:8443/api';
+const BASE_URL = "https://j9b210.p.ssafy.io:8443/api";
 // const navigate = useNavigate();
-// const [currentUser, setCurrentUser] = useRecoilState(CurrentUserAtom)
-// const [isLogin, setLoginState] = useRecoilState(LoginState)
 
 axios.defaults.withCredentials = true;
 
 export const publicApi: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 export const privateApi: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'ngrok-skip-browser-warning': '69420',
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    "ngrok-skip-browser-warning": "69420",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("access_token")}`,
   },
 });
 
+// config에 기존 요청 저장
+// 모든 request요청이 실행되기 전에 호출 -> 모든 요청 헤더에 인증 토큰 추가
 privateApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem('access_token');
-  config.headers.Authorization = 'Bearer ' + token;
-
+  console.log("first config", config);
+  const token = localStorage.getItem("access_token");
+  config.headers.Authorization = "Bearer " + token;
   return config;
 });
 
+// 리프레시 토큰을 통해 서버로부터 새로운 액세스 토근 가져오기
 export async function postRefreshToken() {
-  console.log('리프레시 토큰 재발급');
+  console.log("리프레시 토큰 재발급");
   const headers = {
-    'Authorization-refresh': 'Bearer ' + localStorage.getItem('refresh_token'),
+    withCredential: true,
+    "Authorization-refresh": "Bearer " + localStorage.getItem("refresh_token"),
+    'Access-Control-Allow-Origin': '*',
+    // "Access-Control-Allow-Credentials": true,
   };
-
-  const response = await publicApi.post('/auth/refresh', null, { headers });
+  const response = await publicApi.put("/auth/refresh", null, { headers });
+  console.log("리프리프리프리프");
   return response;
 }
 
+// 모든 응답에 대한 처리
 privateApi.interceptors.response.use(
+  // 응답 성공시
   (response) => {
-    console.log('response');
+    console.log("response");
     return response;
   },
+  // 응답 실패시
 
   async (error) => {
     const { config } = error;
-
-    if (error.response.status === 403) {
-      if (error.response.data.message === '유효하지 않은 토큰') {
-        const originRequest = config;
-        try {
-          const response = await postRefreshToken();
-          const newAccessToken = response.headers['authorization'];
-          localStorage.setItem(
-            'access_token',
-            response.headers['authorization']
-          );
-          localStorage.setItem(
-            'refresh_token',
-            response.headers['authorization-refresh']
-          );
-          axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-          originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-          return axios(originRequest);
-        } catch {
-          localStorage.removeItem('access_token');
-          localStorage.removeItem('refresh_token');
-          window.location.href = ("/");
-        }
+    console.log("에러에러에러");
+    console.log('???',error)
+    if (error.response.status === 401) {
+      const originRequest = config;
+      console.log('401이야?');
+      try {
+        console.log("에러 try");
+        const response = await postRefreshToken();
+        const newAccessToken = response.headers["authorization"];
+        localStorage.setItem("access_token", response.headers["authorization"]);
+        localStorage.setItem(
+          "refresh_token",
+          response.headers["authorization-refresh"]
+        );
+        //
+        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+        originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axios(originRequest);
+      } catch {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        window.location.href = "/";
       }
     }
     return Promise.reject(error);
--- frontend/src/components/Common/LoginHeader.tsx
+++ frontend/src/components/Common/LoginHeader.tsx
@@ -1,6 +1,10 @@
 // 로그인시 헤더
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import swal from "sweetalert";
+import { useRecoilState } from "recoil";
+import { CurrentUserAtom, LoginState } from "../../recoil/Auth";
+useRecoilState
 import {
   Container,
   HeaderWrapper,
@@ -14,11 +18,16 @@ import {
   DropBox,
   ClickBox,
   Content,
+  Hr
 } from "./Header.style";
+
 
 function LoginHeader() {
   const navigate = useNavigate();
   const [isDropBox, setIsDropBox] = useState(false);
+  const [isLogin, setIsLogin] = useRecoilState(LoginState);
+  const [currentUser, setCurrentUser] = useRecoilState(CurrentUserAtom);
+
 
   // 헤더 및 사이드바 조건부 렌더링
   if (window.location.pathname === "/login") return null;
@@ -28,7 +37,14 @@ function LoginHeader() {
   const enterDropBox = () => {
     setIsDropBox(!isDropBox);
     };
-
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    setIsLogin(false); // 로그인 여부 아톰에 저장
+    setCurrentUser(""); // 유저 정보 아톰에 저장
+    swal('로그아웃 완료')
+    navigate("/")
+  }
   return (
     <Container>
       <HeaderWrapper>
@@ -46,8 +62,10 @@ function LoginHeader() {
                 <DropBox>
                   <Img src="./icon/user_purple.png"></Img>
                   <ClickBox>
+                    <Content>{currentUser.nickname}</Content>
+                    <Hr/>
                     <Content onClick={()=>navigate("/mypage")}>마이페이지</Content>
-                    <Content>로그아웃</Content>
+                    <Content onClick={handleLogout}>로그아웃</Content>
                   </ClickBox>
                 </DropBox>
               ) : (
