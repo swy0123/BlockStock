be3344197840c22b7a7db611fb355df9f3982ebc
210-reverof <sheltonwon@gmail.com>
2023-09-21 21:27:05 +0900

    feat: 충전 presentation 레이어 완료

--- backend/member/src/main/java/com/olock/blockstock/member/domain/member/application/MemberService.java
+++ backend/member/src/main/java/com/olock/blockstock/member/domain/member/application/MemberService.java
@@ -2,6 +2,7 @@ package com.olock.blockstock.member.domain.member.application;
 
 import com.olock.blockstock.member.domain.member.dto.request.MemberJoinRequest;
 import com.olock.blockstock.member.domain.member.dto.request.MemberModifyRequest;
+import com.olock.blockstock.member.domain.member.dto.request.MoneyChargeRequest;
 import com.olock.blockstock.member.domain.member.dto.request.PasswordUpdateRequest;
 import com.olock.blockstock.member.domain.member.dto.response.MemberInfoResponse;
 
@@ -12,4 +13,8 @@ public interface MemberService {
 
     void modify(Long memberId, MemberModifyRequest memberModifyRequest);
     void updatePassword(Long memberId, PasswordUpdateRequest passwordUpdateRequest);
+
+    void buyTicket(Long memberId, int ticketCount);
+
+    void chargeMoney(Long memberId, MoneyChargeRequest moneyChargeRequest);
 }
---
+++ backend/member/src/main/java/com/olock/blockstock/member/domain/member/dto/request/MoneyChargeRequest.java
@@ -0,0 +1,10 @@
+package com.olock.blockstock.member.domain.member.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class MoneyChargeRequest {
+    private long money;
+}
