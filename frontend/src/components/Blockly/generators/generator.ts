6dcb3ce60ecf89851cfa301e756016de3fba44ab
이시영 <swy05123@gmail.com>
2023-09-21 16:04:54 +0900

    feat: 블록코딩 페이지 수정 S09P22B210-96

--- frontend/src/components/Blockly/generators/generator.ts
+++ frontend/src/components/Blockly/generators/generator.ts
@@ -8,19 +8,13 @@ import Blockly from 'blockly/core';
 import { Order } from "blockly/javascript";
 import {pythonGenerator} from 'blockly/python';
 
-// Export all the code generators for our custom blocks,
-// but don't register them with Blockly yet.
-// This file has no side effects!
-
  //함수 호출
  pythonGenerator.forBlock['call_function'] = function(block, generator) {
   var value_item = generator.valueToCode(block, 'ITEM', Order.NONE).toString();
   var value_name = generator.valueToCode(block, 'NAME', Order.NONE).toString();
   // var value_item = block.getFieldValue('ITEM');
   // var value_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = value_name+'('+value_item+')';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -29,9 +23,7 @@ import {pythonGenerator} from 'blockly/python';
 pythonGenerator.forBlock['once_open'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
   var field_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = 'data['+value_name+'][0]';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -39,9 +31,7 @@ pythonGenerator.forBlock['once_open'] = function(block, generator) {
 pythonGenerator.forBlock['once_close'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
   var field_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = 'data['+value_name+'][1]';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -49,9 +39,7 @@ pythonGenerator.forBlock['once_close'] = function(block, generator) {
 pythonGenerator.forBlock['once_high'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
   var field_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = 'data['+value_name+'][2]';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -59,9 +47,7 @@ pythonGenerator.forBlock['once_high'] = function(block, generator) {
 pythonGenerator.forBlock['once_low'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
   var field_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = 'data['+value_name+'][3]';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -69,9 +55,7 @@ pythonGenerator.forBlock['once_low'] = function(block, generator) {
 pythonGenerator.forBlock['once_volume'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
   var field_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = 'data['+value_name+'][4]';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -84,7 +68,6 @@ pythonGenerator.forBlock['once_volume'] = function(block, generator) {
 
 pythonGenerator.forBlock['sell'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
-  // TODO: Assemble python into code variable.
   var code = 'sell('+value_name+')';
   return code;
 };
@@ -92,7 +75,6 @@ pythonGenerator.forBlock['sell'] = function(block, generator) {
 
 pythonGenerator.forBlock['buy'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.ATOMIC);
-  // TODO: Assemble python into code variable.
   var code = 'buy('+value_name+')';
   return code;
 };
@@ -100,7 +82,6 @@ pythonGenerator.forBlock['buy'] = function(block, generator) {
 
 pythonGenerator.forBlock['stay'] = function(block, generator) {
   var field_name = block.getFieldValue('NAME');
-  // TODO: Assemble python into code variable.
   var code = 'stay()';
   return code;
 };
@@ -109,14 +90,12 @@ pythonGenerator.forBlock['stay'] = function(block, generator) {
 pythonGenerator.forBlock['minmaxavg_select'] = function(block, generator) {
   var field_name = block.getFieldValue('FIELDNAME');
   // var value_name = generator.valueToCode(block, 'FIELDNAME', Order.NONE);
-  // TODO: Assemble python into code variable.
   var code = field_name;
   return [code, Order.NONE];
 };
 pythonGenerator.forBlock['ochlv_value'] = function(block, generator) {
   var field_name = block.getFieldValue('FIELDNAME');
   // var value_name = generator.valueToCode(block, 'FIELDNAME', Order.NONE);
-  // TODO: Assemble python into code variable.
   var code = field_name;
   return [code, Order.NONE];
 };
@@ -130,7 +109,6 @@ pythonGenerator.forBlock['calculate_scope_data'] = function(block, generator) {
   var value_hla = generator.valueToCode(block, 'HLA', Order.NONE).toString();
   // TODO: Assemble python into code variable.
   var code = 'get_recent_indicators('+value_scope+', '+value_ochl+', '+value_hla+')';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -140,7 +118,6 @@ pythonGenerator.forBlock['date_scope'] = function(block, generator) {
   // var value_date = generator.valueToCode(block, 'DATE', Order.NONE).toString();
   var value_date =  block.getFieldValue('DATE');
   var code = 'date, '+value_date;
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -149,7 +126,6 @@ pythonGenerator.forBlock['term_scope'] = function(block, generator) {
   // var value_date = generator.valueToCode(block, 'DATE', Order.NONE).toString();
   var value_date =  block.getFieldValue('DATE');
   var code = 'term, '+value_date;
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -158,7 +134,6 @@ pythonGenerator.forBlock['cur_data'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'OCHL', Order.NONE).toString();
   // TODO: Assemble python into code variable.
   var code = 'curData('+value_name+')';
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Order.NONE];
 };
 
@@ -167,8 +142,6 @@ pythonGenerator.forBlock['cnt_per_asset'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.NONE).toString();
   // TODO: Assemble python into code variable.
   var code = 'asset('+value_name+', per)';
-  // TODO: Change ORDER_NONE to the correct strength.
-  // return [code, Order.NONE];
   return [code, Order.NONE];
 };
 
@@ -178,8 +151,6 @@ pythonGenerator.forBlock['cnt_per_reserve'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', Order.NONE).toString();
   // TODO: Assemble python into code variable.
   var code = 'reserves('+value_name+', per)';
-  // TODO: Change ORDER_NONE to the correct strength.
-  // return [code, Order.NONE];
   return [code, Order.NONE];
 };
 
