a86417281000072869f692755120db109756fc43
210-reverof <sheltonwon@gmail.com>
2023-09-22 15:11:06 +0900

    fix: kafka consumer 연결 완료

--- backend/tactic/main.py
+++ backend/tactic/main.py
@@ -1,17 +1,20 @@
 import os
 
 import uvicorn
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from db.conn import engineconn
 from domain.tactic.routers import tactic
 from domain.contest.routers import contest
 from domain.option.routers import option
 from redis_config import redis_config
+import asyncio
+import infra.kafka.member_consumer as member_consumer
 
 import py_eureka_client.eureka_client as eureka_client
 
 app = FastAPI(host="0.0.0.0", port=8000)
+route = APIRouter()
 
 origins = [
     "http://localhost:5173/",
@@ -26,6 +29,9 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"]
 )
+
+app.include_router(member_consumer.route)
+asyncio.create_task(member_consumer.consume())
 
 
 @app.options("/{path:path}", status_code=status.HTTP_200_OK)
@@ -48,6 +54,7 @@ async def startup_event():
 
 engine = engineconn()
 session = engine.sessionmaker()
+
 
 
 @app.get("/")
