36db525dd56e41eca98564db8be3e17005dbf9e5
hajin618 <1998rlagkwls@gmail.com>
2023-09-22 23:29:34 +0900

    feat: get_recent_indicators 수정 S09P22B210-172

--- backend/tactic/domain/tactic/routers/tactic.py
+++ backend/tactic/domain/tactic/routers/tactic.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from domain.tactic.schemas.tactic_test_request import TacticTestRequest
 from domain.tactic.schemas.tactic_test_response import TacticTestResponse
-from domain.tactic.services.tactic_service import get_data_list, get_tactic_test_response
+from domain.tactic.services.tactic_service import get_tactic_test_response
 
 app = APIRouter(
     prefix="/tactic"
@@ -13,13 +13,5 @@ app = APIRouter(
 async def tactic_test(tactic_test_request: TacticTestRequest):
     # print(tactic_test_request.tacticPythonCode)
     response = get_tactic_test_response(tactic_test_request)
-
-    # i = 0
-    #
-    #
-    # for i in range(tactic_test_request.repeatCnt):
-    #     if (get_recent_indicators(10, "low", "avg")) == (curData("low")):
-    #         buy(("10%"))
-    #     i += 1
 
     return response
