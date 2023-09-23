626402091cafa578e1234a2144c5e05592889a7f
hajin618 <1998rlagkwls@gmail.com>
2023-09-23 23:20:51 +0900

    fix: tacticJsonCode str -> object

--- backend/tactic/domain/tactic/schemas/tactic_test_request.py
+++ backend/tactic/domain/tactic/schemas/tactic_test_request.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 class TacticTestRequest(BaseModel):
     optionCode: Optional[str] = None
     tacticPythonCode: Optional[str] = None
-    tacticJsonCode: Optional[str] = None
+    tacticJsonCode: Optional[object] = None
     startAsset: Optional[int] = None
     startTime: Optional[str] = None
     term: Optional[str] = None
