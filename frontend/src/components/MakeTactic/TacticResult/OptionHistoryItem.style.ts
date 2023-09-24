a4e3b3bc62a20495fb54680703e42c9a25c45716
이시영 <swy05123@gmail.com>
2023-09-25 00:13:26 +0900

    feat: 전략 저장 화면 추가

--- frontend/src/components/MakeTactic/TacticResult/OptionHistoryItem.style.ts
+++ frontend/src/components/MakeTactic/TacticResult/OptionHistoryItem.style.ts
@@ -1,11 +1,45 @@
 import styled from "styled-components";
 
 export const ItemContainer = styled.div`
-position: relative;
-    width: 100%;
-    height: 20%;
-    font-size: 15px;
-    margin-bottom: 2px;
-    border-radius: 5px;
-    border-style: solid;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  width: 100%;
+  height: 70px;
+  font-size: 15px;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 10px;
+`;
+
+export const TermImg = styled.img`
+  width: 13px;
+  height: 13px;
+  margin-right: 5px;
+  /* margin: 7px 0px 0px 0px; */
+`;
+
+export const TermContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  margin-left: 10px;
+  height: 20px;
+  width: 60px;
+  margin-right: 15px;
+  border-style: solid;
+  border-width: 1px;
+  color: #9c9c9c;
+  
+`;
+
+export const OptionHistoryItemContent = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  text-align: center;
+  align-items: center;
 `;
