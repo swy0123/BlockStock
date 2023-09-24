c343583eaeb070321cdf976f5fc110aa590a3b67
김민균 <rlaalsrbs15@naver.com>
2023-09-24 21:35:28 +0900

    feat: contest

---
+++ frontend/src/pages/Contest/ContestProgress.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ContestProgress(){
+  return(
+    <>
+    <Container>
+      <h1>대회 진행 현황</h1>
+    </Container>
+    
+    </>
+  )
+}
+
+export default ContestProgress
+
+const Container = styled.div`
+width: 1000px;
+height: 700px;
+border: 1px solid black;
+`;
