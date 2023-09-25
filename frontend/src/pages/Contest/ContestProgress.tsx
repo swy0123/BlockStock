ea446d263c88710cb3e7f9a17b805d913e9b081f
이시영 <swy05123@gmail.com>
2023-09-25 23:47:00 +0900

    feat: 대회 상세 화면 수정 S09P22B210-244

--- frontend/src/pages/Contest/ContestProgress.tsx
+++ frontend/src/pages/Contest/ContestProgress.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { TacticContainer } from "../MakeTactic/MakeTactic.style";
 import ContestTacticResult from "../../components/Contest/ContestProgress/ContestTacticResult";
-import { useLocation, useEffect } from "react-router-dom";
+import { useLocation, } from "react-router-dom";
 
 function ContestProgress() {
   const location = useLocation();
