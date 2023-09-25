cda1b07688b9dbd7b961c412ddd839890d639a85
hajin618 <1998rlagkwls@gmail.com>
2023-09-25 14:17:42 +0900

    feat: S3 파일 업로드 확인 S09P22B210-147

---
+++ backend/board/src/main/java/com/olock/blockstotck/board/global/AwsS3/FileController.java
@@ -0,0 +1,23 @@
+package com.olock.blockstotck.board.global.AwsS3;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class FileController {
+
+    private final AwsS3Uploader awsS3Uploader;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        String fileName = awsS3Uploader.upload(multipartFile, "test");
+        return fileName;
+    }
+}
