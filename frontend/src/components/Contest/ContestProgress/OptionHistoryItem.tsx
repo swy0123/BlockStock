fd464245060f76925b74669f36bc59bba0f23ffd
이시영 <swy05123@gmail.com>
2023-09-25 22:55:50 +0900

    fix: 충돌 해결

---
+++ frontend/src/components/Contest/ContestProgress/OptionHistoryItem.tsx
@@ -0,0 +1,67 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { format } from "d3-format";
+import {
+  ItemContainer,
+  OptionHistoryItemContent,
+  TermContainer,
+  TermImg,
+} from "./OptionHistoryItem.style";
+import TermImgSrc from "../../../assets/img/MakeTactic/term.png";
+import {
+  OptionHistoryItemPosCenter,
+  OptionHistoryItemPosDown,
+  OptionHistoryItemPosLeft,
+  OptionHistoryItemPosRight,
+  OptionHistoryItemPosUp,
+} from "./ContestTacticResult.style";
+import { colors } from "@mui/material";
+//     {item.optioncode}
+//     {item.optionname}
+//     {item.currate}
+//     {item.cost}
+const OptionHistoryItem = (props) => {
+  //   const [optioncode, setOptioncode] = useState(props.optioncode);
+  //   const [optionname, setOtionname] = useState(props.optionname);
+  //   const [currate, setCurrate] = useState(props.currate);
+  //   const [cost, setCost] = useState(props.cost);
+  const numberDisplayFormat = format(",");
+  return (
+    <ItemContainer>
+      <TermContainer>
+        <TermImg src={TermImgSrc} />
+        {(props.item.date).replace(/^(\d{4})(\d{2})(\d{2})$/, '$2/$3')} {(props.item.time).replace(/^(\d{2})(\d{2})$/, '$1:$2')}
+      </TermContainer>
+      <OptionHistoryItemContent>
+        {props.item.type == "buy" ? (
+          <OptionHistoryItemPosLeft style={{ color: "#EC4275" }}>매수</OptionHistoryItemPosLeft>
+        ) : (
+          <OptionHistoryItemPosLeft style={{ color: "#097DF3" }}>매도</OptionHistoryItemPosLeft>
+        )}
+        <OptionHistoryItemPosCenter>
+          <OptionHistoryItemPosUp>
+            {numberDisplayFormat(props.item.cost)}({numberDisplayFormat(props.item.tradeCnt)}주)
+          </OptionHistoryItemPosUp>
+          <OptionHistoryItemPosDown>
+            {numberDisplayFormat(props.item.cost * props.item.tradeCnt * 0.00015)}
+          </OptionHistoryItemPosDown>
+        </OptionHistoryItemPosCenter>
+        <OptionHistoryItemPosRight>
+          <OptionHistoryItemPosUp>{props.item.tradeCnt * props.item.cost}</OptionHistoryItemPosUp>
+          <OptionHistoryItemPosDown>
+            {props.item.type === "sell" ? (
+              props.item.profitAndLoss > 0 ? (
+                <span style={{ color: "#EC4275" }}>+{props.item.profitAndLoss}</span>
+              ) : (
+                <span style={{ color: "#097DF3" }}>{props.item.profitAndLoss}</span>
+              )
+            ) : (
+              <>&nbsp;</>
+            )}
+          </OptionHistoryItemPosDown>
+        </OptionHistoryItemPosRight>
+      </OptionHistoryItemContent>
+    </ItemContainer>
+  );
+};
+
+export default OptionHistoryItem;
