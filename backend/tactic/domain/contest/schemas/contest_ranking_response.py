fdd698ce1ed9060090559d4c3d4ab03a3becd724
KodaHye <kodhdh09@gmail.com>
2023-09-26 12:58:33 +0900

    feat: 특정 대회 랭킹 반환 S09P22B210-230

--- backend/tactic/domain/contest/schemas/contest_ranking_response.py
+++ backend/tactic/domain/contest/schemas/contest_ranking_response.py
@@ -1,10 +1,12 @@
-class ContestPrevResponse:
+class ContestRankingResponse:
     memberId: int
+    nickName: str
     profileImage: str
     returns: float
 
-    def __init__(self, member_id: int, profile_image: str, ticket: int, result_money: float):
+    def __init__(self, member_id: int, nick_name: str, profile_image: str, ticket: int, result_money: float):
         self.memberId = member_id
+        self.nickName = nick_name
         self.profileImage = profile_image
         self.returns = (result_money - ticket * 10000000) / (ticket * 10000000) * 100
 
