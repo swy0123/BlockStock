c918df69230509ded71b0baf39bef9e30a5f30e2
KodaHye <kodhdh09@gmail.com>
2023-09-26 23:29:45 +0900

    feat: 메인화면 주식 목록 조회 반환 이름 수정 S09P22B210-229

--- backend/tactic/domain/option/schemas/option_response.py
+++ backend/tactic/domain/option/schemas/option_response.py
@@ -8,7 +8,7 @@ class OptionResponse:
     comparePrevious: str  # 전일 대비율
 
     def __init__(self, option_code: str, option_name: str, currentPrice: str, comparePrevious: str):
-        self.option_code = option_code
-        self.option_name = option_name
+        self.optionCode = option_code
+        self.optionName = option_name
         self.currentPrice = currentPrice
         self.comparePrevious = comparePrevious
