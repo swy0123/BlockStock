1d70e834220475ffdde1b689029a51e3355e634f
이시영 <swy05123@gmail.com>
2023-09-26 17:03:45 +0900

    Update ContestProgress.tsx

--- frontend/src/pages/Contest/ContestProgress.tsx
+++ frontend/src/pages/Contest/ContestProgress.tsx
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-import React, {useEffect} from "react";
 import { TacticContainer } from "../MakeTactic/MakeTactic.style";
 import ContestTacticResult from "../../components/Contest/ContestProgress/ContestTacticResult";
 import { useLocation } from "react-router-dom";
-
-=======
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { TacticContainer } from "../MakeTactic/MakeTactic.style";
-import ContestTacticResult from "../../components/Contest/ContestProgress/ContestTacticResult";
-import { useLocation, } from "react-router-dom";
->>>>>>> 257736c7c50927b421a37bc32b695952507a9158
 
 function ContestProgress() {
   const location = useLocation();
