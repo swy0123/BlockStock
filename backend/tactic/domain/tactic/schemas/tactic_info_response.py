4c66cc8f1881b27c8deeb7d93178aa8fc5d4d1aa
210-reverof <sheltonwon@gmail.com>
2023-09-26 16:37:45 +0900

    fix: 전략 상세 조회 완료

--- backend/tactic/domain/tactic/schemas/tactic_info_response.py
+++ backend/tactic/domain/tactic/schemas/tactic_info_response.py
@@ -4,6 +4,7 @@ from datetime import datetime
 class TacticInfoResponse:
     id: int
     title: str
+    optionCode: str
     optionName: str
     testReturns: float
     contestReturns: float
@@ -13,6 +14,7 @@ class TacticInfoResponse:
     def __init__(self, tactic, option_name):
         self.id = tactic.id
         self.title = tactic.title
+        self.optionCode = tactic.option_code
         self.optionName = option_name
         self.testReturns = tactic.test_returns
         self.contestReturns = tactic.contest_returns
