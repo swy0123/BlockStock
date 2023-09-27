cd268390093b19868fb0c632f2c75d3d1ea1692f
김민균 <rlaalsrbs15@naver.com>
2023-09-27 10:13:28 +0900

    feat: 대회 결과

--- frontend/src/components/Contest/ContestStore/CompletedContest/CompletedContestModal.tsx
+++ frontend/src/components/Contest/ContestStore/CompletedContest/CompletedContestModal.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
 import CloseIcon from '@mui/icons-material/Close';
 import { useRecoilValue } from "recoil";
-import { useRank } from "../../../../recoil/Contest/CompletedContest";
 import style from './Carousel.module.css'
 import {
     Container,
@@ -21,8 +20,11 @@ import {
     NoRankImage,
 } from './CompletedContestModal.style'
 
-function CompletedContestModal({onClose, selectedContest}){
-    const rankList = useRecoilValue(useRank);
+function CompletedContestModal({onClose, selectedContest, rank}){
+
+    useEffect(()=>{
+      console.log(rank,'랭킹 모달')
+    },[])
 
     return(
         <Container>
@@ -31,66 +33,99 @@ function CompletedContestModal({onClose, selectedContest}){
                     <Title>[경진대회] {selectedContest.title}</Title>
                     <CloseBtn onClick={onClose}><CloseIcon/></CloseBtn>
                 </Header>
-                <Schedule>대회 기간: {selectedContest.startAt} ~ {selectedContest.endAt}</Schedule>
+                <Schedule>대회 기간: {selectedContest.startTime} ~ {selectedContest.endTime}</Schedule>
                 <Personnel>참가 인원 수 {selectedContest.joinPeople} / {selectedContest.maxCapacity} </Personnel>
-
+                {rank.length === 0 ? (
+                  <div>유저가 없습니다</div>
+                ) : (
                 <Wrapper>
                     <RankUser>
                     <div className={style.carousel}>
                         <div className={style.carousel_content}>
                         <div className="carousel-item" style={{width: '170px', height: '220px'}}>
-                            <RankImage src={
-                                // contest.ranking[0].profileImage ||
-                                '/icon/user_purple.png'} />
-                                <h2 style={{textAlign: 'center', margin: '5px 0px 0px 0px' , fontSize:'22px'}}>1등</h2>
-                                <RankUserNickName>
-                                    {rankList[0].nickName}
-                                </RankUserNickName>
-                                <div style={{textAlign: 'center', display:'flex'}}>
-                                  <div style={{color:'#8A8A8A', margin:'8px 5px 0px 50px', fontSize:'12px'}}>
-                                      수익률 :
-                                  </div>
-                                <ContestReturn style={{ color: rankList[0].return[0] === '-' ? 'blue' : 'red' }}>
-                                    {rankList[0].return}
-                                </ContestReturn>
-                                </div>
-                            </div>
-                        <div className="carousel-item" style={{width: '170px', height: '220px'}}>
-                            <RankImage src={
-                                // contest.ranking[1].profileImage ||
-                                '/icon/user_purple.png'} />
-                                <h2 style={{textAlign: 'center', margin: '5px 0px 0px 0px', fontSize:'22px'}}>2등</h2>
-                                <RankUserNickName>
-                                    {rankList[1].nickName}
-                                </RankUserNickName>
-
-                                <div style={{textAlign: 'center', display:'flex'}}>
-                                  <div style={{color:'#8A8A8A', margin:'8px 5px 0px 50px', fontSize:'12px'}}>
-                                      수익률 :
-                                  </div>
-                                  <ContestReturn style={{ color: rankList[1].return[0] === '-' ? 'blue' : 'red' }}>
-                                      {rankList[1].return}
+                            {rank[0] ? (
+                              <div>유저가 없습니다</div>
+                            ) : (
+                              <>
+                              <RankImage src={
+                                  // contest.ranking[0].profileImage ||
+                                  '/icon/user_purple.png'} />
+                                  <h2 style={{textAlign: 'center', margin: '5px 0px 0px 0px' , fontSize:'22px'}}>1등</h2>
+                                  <RankUserNickName>
+                                    {/* {rank[0]?.nickName ? (
+                                      <>{rank[0].id}</>
+                                    ) : (
+                                      <>NickName이 없습니다.</>
+                                    )} */}
+                                  </RankUserNickName>
+                                  <div style={{textAlign: 'center', display:'flex'}}>
+                                    <div style={{color:'#8A8A8A', margin:'8px 5px 0px 50px', fontSize:'12px'}}>
+                                        수익률 :
+                                    </div>
+                                  <ContestReturn style={{ color: rank[0].returns === '-' ? 'blue' : 'red' }}>
+                                      {rank[0].returns}
                                   </ContestReturn>
-                                </div>
+                                  </div>
+                              </>
+                            )}
+                            </div>
+
+                        <div className="carousel-item" style={{width: '170px', height: '220px'}}>
+                              {rank[1] ? (
+                                <div>유저가 없습니다</div>
+                              ) : (
+                                <>
+                                <RankImage src={
+                                    // contest.ranking[1].profileImage ||
+                                    '/icon/user_purple.png'} />
+                                    <h2 style={{textAlign: 'center', margin: '5px 0px 0px 0px', fontSize:'22px'}}>2등</h2>
+                                    <RankUserNickName>
+                                    {/* {rank[1].nickName ? (
+                                      <>{rank[1].nickName}</>
+                                    ) : (
+                                      <>NickName이 없습니다.</>
+                                    )} */}
+                                  </RankUserNickName>
+                                    <div style={{textAlign: 'center', display:'flex'}}>
+                                      <div style={{color:'#8A8A8A', margin:'8px 5px 0px 50px', fontSize:'12px'}}>
+                                          수익률 :
+                                      </div>
+                                      <ContestReturn style={{ color: rank[1].returns === '-' ? 'blue' : 'red' }}>
+                                          {rank[1].returns}
+                                      </ContestReturn>
+                                    </div>
+                                </>
+                              )}
                                 
                         </div>
                         <div className="carousel-item" style={{width: '170px', height: '220px'}}>
-                            <RankImage src={
-                                // contest.ranking[2].profileImage ||
-                                '/icon/user_purple.png'} />
-                                <h2 style={{textAlign: 'center', margin: '5px 0px 0px 0px', fontSize:'22px' }}>3등</h2>
-                                <RankUserNickName>
-                                    {rankList[2].nickName}
-                                </RankUserNickName>
-                                <div style={{textAlign: 'center', display:'flex'}}>
-                                <div style={{color:'#8A8A8A', margin:'8px 5px 0px 50px', fontSize:'12px'}}>
-                                    수익률 :
-                                </div>
-                                <ContestReturn style={{ color: rankList[2].return[0] === '-' ? 'blue' : 'red' }}>
-                                    {rankList[2].return}
-                                </ContestReturn>
-                                </div>
-                        </div>
+                          {rank[2] ? (
+                            <div>유저가 없습니다</div>
+                          ) : (
+                            <>
+                              <RankImage src={
+                                  // contest.ranking[2].profileImage ||
+                                  '/icon/user_purple.png'} />
+                                  <h2 style={{textAlign: 'center', margin: '5px 0px 0px 0px', fontSize:'22px' }}>3등</h2>
+                                  <RankUserNickName>
+                                    {/* {rank[2]?.nickName ? (
+                                      <>{rank[2].nickName}</>
+                                    ) : (
+                                      <>NickName이 없습니다.</>
+                                    )} */}
+                                  </RankUserNickName>
+                                  <div style={{textAlign: 'center', display:'flex'}}>
+                                    <div style={{color:'#8A8A8A', margin:'8px 5px 0px 50px', fontSize:'12px'}}>
+                                        수익률 :
+                                    </div>
+                                    <ContestReturn style={{ color: rank[2].returns === '-' ? 'blue' : 'red' }}>
+                                        {rank[2].returns}
+                                    </ContestReturn>
+                                  </div>
+                                </>
+                              )}
+
+                          </div>
                         </div>
                     </div>
                     </RankUser>
@@ -98,33 +133,44 @@ function CompletedContestModal({onClose, selectedContest}){
                     <Line/>
 
                   <Participant>
-                    {rankList.slice(3).map((participant, participantIndex) => (
-                      <div key={participantIndex}>
-                        <div style={{margin:'0px 0px 0px 10px', display:'flex'}}>
-                          <div style={{width:'40px', margin:'10px 0px 5px 15px', color:'#8A8A8A', fontSize:'15px'}}>
-                            {participantIndex+4}등
-                          </div>
-                          <NoRankImage src={
-                            // participant.profileImage ||
-                            '/icon/user_purple.png'} />
-                          <div style={{margin:'10px 15px 0px 0px', fontSize:'12px', fontWeight:'bold', width:'50px'}}>
-                            {participant.nickName}
-                          </div>
-                          <div style={{display:'flex', marginTop:'12px', fontSize:'10px'}}>
-                            <div style={{color:'#8A8A8A', marginRight:'10px'}}>
-                              수익률 : 
+                    {rank.length < 4 ? (
+                      <></>
+                    ) : (
+                      <>
+                      {rank.slice(3).map((participant, participantIndex) => (
+                        <div key={participantIndex}>
+                          <div style={{margin:'0px 0px 0px 10px', display:'flex'}}>
+                            <div style={{width:'40px', margin:'10px 0px 5px 15px', color:'#8A8A8A', fontSize:'15px'}}>
+                              {participantIndex+4}등
                             </div>
-                            <div style={{ color: participant.return[0] === '-' ? 'blue' : 'red', fontWeight:'bold', fontSize: '12px' }}>
-                              {participant.return}
+                            <NoRankImage src={
+                              // participant.profileImage ||
+                              '/icon/user_purple.png'} />
+                            <div style={{margin:'10px 15px 0px 0px', fontSize:'12px', fontWeight:'bold', width:'50px'}}>
+                              {/* {participant.nickName ? (
+                                <></>
+                              ) : (
+                                <>{participant.nickName}</>
+                              )} */}
+                            </div>
+                            <div style={{display:'flex', marginTop:'12px', fontSize:'10px'}}>
+                              <div style={{color:'#8A8A8A', marginRight:'10px'}}>
+                                수익률 : 
+                              </div>
+                              <div style={{ color: participant.returns === '-' ? 'blue' : 'red', fontWeight:'bold', fontSize: '12px' }}>
+                                {participant.returns}
+                              </div>
                             </div>
                           </div>
+                          <hr style={{width:'270px'}}/>
                         </div>
-                        <hr style={{width:'270px'}}/>
-                      </div>
-                    ))}
+                      ))}
+                      </>
+                    )}
                   </Participant>
 
                 </Wrapper>
+                )}
             </Modal>
         </Container>
     )
