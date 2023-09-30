4c0b0c0d4335e5e6d298273cd8b327fc9475d66d
김민균 <rlaalsrbs15@naver.com>
2023-09-30 23:45:15 +0900

    style: tacticboard

--- frontend/src/components/TacticBoard/TacticBoardTitle.tsx
+++ frontend/src/components/TacticBoard/TacticBoardTitle.tsx
@@ -6,13 +6,22 @@ font-weight: 400;
 font-size: 25px;
 color: #000000;
 font-weight: bold;
+display: flex;
 `;
-
+const Line = styled.div`
+width: 80%;
+height: 1px;
+background-color: #B6ABBB;
+margin: 16px 0px 0px 40px;
+`;
 
 function TacticBoardTitle(){
   return(
     <Container>
-      전략게시판
+      <div>
+        전략게시판
+      </div>
+      <Line />
     </Container>
   )
 }
--- frontend/src/pages/TacticBoard/TacticBoardList.tsx
+++ frontend/src/pages/TacticBoard/TacticBoardList.tsx
@@ -4,12 +4,8 @@ import TacticBoardTitle from "../../components/TacticBoard/TacticBoardTitle";
 import TacticBoardBox from "../../components/TacticBoard/TacticBoardList/TacticBoardBox";
 
 const Container = styled.div`
-  /* margin: 30px 0px 0px 80px; */
   width: 100%;
   height: 600px;
-  display: flex;
-  justify-content: center;
-  text-align: center;
 `;
 
 function TacticBoardList() {
