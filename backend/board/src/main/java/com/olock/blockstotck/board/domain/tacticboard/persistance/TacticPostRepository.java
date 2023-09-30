f445774698acaa6862286773916ca6487fd872dd
KodaHye <kodhdh09@gmail.com>
2023-10-01 02:52:16 +0900

    fix: 레포지토리 내 updateHit 함수 삭제

--- backend/board/src/main/java/com/olock/blockstotck/board/domain/tacticboard/persistance/TacticPostRepository.java
+++ backend/board/src/main/java/com/olock/blockstotck/board/domain/tacticboard/persistance/TacticPostRepository.java
@@ -2,11 +2,8 @@ package com.olock.blockstotck.board.domain.tacticboard.persistance;
 
 import com.olock.blockstotck.board.domain.tacticboard.persistance.entity.TacticPost;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TacticPostRepository extends JpaRepository<TacticPost, Long> {
-    @Query(value = "UPDATE TacticPost t set t.hit = t.hit + 1 WHERE t.id = :tacticPostId")
-    void updateHit(Long tacticPostId);
 }
