4965483240b7f6187f1d447278514a61621f8aad
210-reverof <sheltonwon@gmail.com>
2023-09-30 20:18:33 +0900

    fix: 대회 history 반환 값 수정

--- backend/tactic/domain/contest/schemas/contest_history_response.py
+++ backend/tactic/domain/contest/schemas/contest_history_response.py
@@ -9,8 +9,8 @@ class ContestHistoryResponse:
     def __init__(self, participate_id: int, title: str, result_money: int, tactic_id: int, rank: int, ticket: int):
         self.title = title
         self.rank = rank
-        self.result_money = result_money
-        self.revenue = result_money / (10000000 * ticket) * 100
+        self.result_money = result_money - (ticket * 10000000)
+        self.revenue = (result_money - ticket * 10000000) / (10000000 * ticket) * 100
         self.tactic_id = tactic_id
         self.participate_id = participate_id
 
