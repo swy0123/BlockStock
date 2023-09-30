27b4fdd297981ced748d7f4acbe6f3db1d0c2c0a
이시영 <swy05123@gmail.com>
2023-09-30 16:38:53 +0900

    feat: 블록 코딩 화면 수정 S09P22B210-235

--- frontend/src/components/Blockly/BlocklyComponent.style.ts
+++ frontend/src/components/Blockly/BlocklyComponent.style.ts
@@ -5,14 +5,27 @@ export const BlocklyWrapper = styled.div`
   width: 100%;
 `;
 export const BlocklyDiv = styled.div`
-  width: 96%;
-  height: 96%;
+  /* width: 96%;
+  height: 96%; */
   position: absolute;
-  top:2%;
-  left: 2%;
+  /* top:2%;
+  left: 2%; */
   /* border: 1px solid transparent;
   border-radius: 6px; */
 `;
+export const BlocklyArea = styled.div`
+  height: 95%;
+  width: 100%;
+  margin-bottom: 5%;
+`;
+export const CollapseToolBoxButton = styled.div`
+  position: absolute;
+  bottom: 5%;
+  z-index: 100;
+  width: 128px;
+  text-align: center;
+`;
+
 // export const BlocklyTreeLabel = styled.div`
 //   color: white;
 // `;
