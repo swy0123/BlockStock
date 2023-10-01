c0d9548b144cfa1d5e61631ec363d4dcb569379c
KodaHye <kodhdh09@gmail.com>
2023-10-01 23:08:47 +0900

    fix: BaseEntity 수정

--- backend/board/src/main/java/com/olock/blockstotck/board/Application.java
+++ backend/board/src/main/java/com/olock/blockstotck/board/Application.java
@@ -3,9 +3,11 @@ package com.olock.blockstotck.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
