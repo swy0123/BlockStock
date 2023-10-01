6e094e4f9c60939048ba2bc0ae365bd6dbeaa7da
hajin618 <1998rlagkwls@gmail.com>
2023-10-01 22:07:51 +0900

    feat: 자유게시글 작성 api S09P22B210-191

--- backend/board/src/main/java/com/olock/blockstotck/board/domain/freeboard/application/FreeboardService.java
+++ backend/board/src/main/java/com/olock/blockstotck/board/domain/freeboard/application/FreeboardService.java
@@ -1,8 +1,18 @@
 package com.olock.blockstotck.board.domain.freeboard.application;
 
+import com.olock.blockstotck.board.domain.freeboard.dto.request.FreeboardPostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 
 public interface FreeboardService{
+
+    long postFreePost(long memberId, FreeboardPostRequest freeboardPostRequest);
+
+    void postFreeBoardFile(List<MultipartFile> multipartFileList,
+                           List<String> filePathList,
+                           long freePostId);
 }
--- backend/board/src/main/java/com/olock/blockstotck/board/domain/freeboard/presentation/FreeboardController.java
+++ backend/board/src/main/java/com/olock/blockstotck/board/domain/freeboard/presentation/FreeboardController.java
@@ -1,9 +1,16 @@
 package com.olock.blockstotck.board.domain.freeboard.presentation;
 
 import com.olock.blockstotck.board.domain.freeboard.application.FreeboardService;
+import com.olock.blockstotck.board.domain.freeboard.dto.request.FreeboardPostRequest;
+import com.olock.blockstotck.board.infra.awsS3.AwsS3Uploader;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/free-board")
@@ -11,4 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class FreeboardController {
 
     private final FreeboardService freeboardService;
+    private final AwsS3Uploader awsS3Uploader;
+
+    @PostMapping("")
+    public ResponseEntity<Void> postFreePost(@RequestHeader("Member-id") Long memberId,
+                                     @RequestPart(value = "fileList") List<MultipartFile> multipartFileList,
+                                     @RequestPart(value = "postRequest") FreeboardPostRequest freeboardPostRequest) throws IOException {
+
+        long freePostId = freeboardService.postFreePost(memberId, freeboardPostRequest);
+
+        List<String> filePathList = new ArrayList<>();
+        for(MultipartFile file : multipartFileList){
+            String filePath = awsS3Uploader.upload(file, "freeboard");
+            filePathList.add(filePath);
+        }
+
+        freeboardService.postFreeBoardFile(multipartFileList, filePathList, freePostId);
+
+        return ResponseEntity.ok().build();
+    }
 }
