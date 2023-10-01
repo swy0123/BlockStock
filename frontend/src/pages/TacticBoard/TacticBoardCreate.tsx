d1a6218a79c71a55535fe234d96a6fc5eab5aa50
김민균 <rlaalsrbs15@naver.com>
2023-10-01 10:15:17 +0900

    style: create

--- frontend/src/pages/TacticBoard/TacticBoardCreate.tsx
+++ frontend/src/pages/TacticBoard/TacticBoardCreate.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import TacticBoardCreateBox from "../../components/TacticBoard/TacticBoardCreate/TacticBoardCreateBox";
 const Container = styled.div`
-  margin: 50px 0px 0px 50px;
 `;
 function TacticBoardCreate(){
   return(
