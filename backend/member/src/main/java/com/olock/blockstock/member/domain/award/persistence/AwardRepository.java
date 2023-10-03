e6575ce8253ca3bb62850d04f62b4890532f3af2
210-reverof <sheltonwon@gmail.com>
2023-10-04 01:03:48 +0900

    feat: 칭호 레포지토리 메서드 추가

--- backend/member/src/main/java/com/olock/blockstock/member/domain/award/persistence/AwardRepository.java
+++ backend/member/src/main/java/com/olock/blockstock/member/domain/award/persistence/AwardRepository.java
@@ -2,7 +2,16 @@ package com.olock.blockstock.member.domain.award.persistence;
 
 import com.olock.blockstock.member.domain.award.persistence.entity.Award;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AwardRepository extends Neo4jRepository<Award, Long> {
 
+    @Query("MATCH (m:Member) WHERE m.id = $memberId CREATE (m)-[:have]->(a:Award {contestId: $contestId, name: $name}) RETURN m, a")
+    void addReward(@Param("memberId") Long memberId, @Param("contestId") Long contestId, @Param("name") String name);
+
+    @Query("MATCH (m:Member)-[:have]->(a:Award) WHERE m.id = $memberId RETURN COLLECT(a.name) AS names")
+    List<String> findAwardTitlesByMemberId(@Param("memberId") Long memberId);
 }
