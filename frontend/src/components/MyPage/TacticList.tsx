e36c5364208ad9fa088601a96c430ff92d894695
yunie <wldbs8241@naver.com>
2023-10-04 02:49:38 +0900

    fix: 마이페이지 전략 목록 수정

--- frontend/src/components/MyPage/TacticList.tsx
+++ frontend/src/components/MyPage/TacticList.tsx
@@ -7,7 +7,8 @@ import { useQuery } from "react-query";
 import { getTactic } from "../../api/MyPage/Mypage";
 
 const StyledSlider = styled(Slider)`
-  width: 90%;
+  width: 100%;
+  padding: 0px 10px;
 `
 const Container = styled.div`
   /* justify-content: center; */
@@ -17,12 +18,16 @@ const Container = styled.div`
 `;
 
 const TacticCard = styled.div`
-  width: 200px; // 각 슬라이드의 넓이
+  width: 70%; // 각 슬라이드의 넓이
   height: 280px;
   /* flex-shrink: 0; */
   border-radius: 13px;
   background: #fff;
   box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.08);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 `;
 
 const Space = styled.div`
@@ -53,6 +58,31 @@ const Noneimg = styled.img`
   height: 30px;
   margin-right: 10px;
 `
+const Medal = styled.img`
+  width: 25px;
+  height: 25px;
+`
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const TacticImg = styled.img`
+  width: 80%;
+  height: 120px;
+  border-radius: 6px;
+`
+const Name = styled.p`
+  font-size: 15px;
+  margin-bottom: 5px;
+`
+const Return = styled.p`
+  font-size: 13px;
+  margin-bottom: 5px;
+`
+const Date = styled.p`
+  font-size: 13px;
+`
 function TacticList() {
   const {data, isLoading, isError} = useQuery("mytactic", getTactic);
 
@@ -60,8 +90,8 @@ function TacticList() {
   const settings = {
     dots: false,
     infinite: true,
-    speed: 500,
-    slidesToShow: 5, // 슬라이드를 3개로 줄임
+    speed: 400,
+    slidesToShow: 3, // 슬라이드를 3개로 줄임
     slidesToScroll: 1,
     // centerPadding: '20px', // centerPadding을 제거
   };
@@ -75,12 +105,18 @@ function TacticList() {
     <Container>
       {data && data.length > 0 ? (
       <Wrapper>
-      <StyledSlider>
+      <StyledSlider {...settings}>
         {data.map((item) => (
-          <Space>
-          <TacticCard>
-            <Title>{item.title}</Title>
-            <Title>{item.testReturns}</Title>
+          <Space >
+          <TacticCard >
+            <Box>
+              <Medal src="/icon/medal2.png"/>
+              <Title>{item.title}</Title>
+            </Box>
+            <TacticImg src="https://firebasestorage.googleapis.com/v0/b/pocket-sch.appspot.com/o/tmp_block.png?alt=media&token=01a14d47-374b-4c7f-93af-1b902bad2031&_gl=1*1w4ri8m*_ga*MjIwNzM4OS4xNjc2OTA3ODQ2*_ga_CW55HF8NVT*MTY5NjMxNjM2Ni41LjEuMTY5NjMxNjU1NS4zNi4wLjA."/>
+            <Name>{item.optionName}</Name>
+            <Return>{item.testReturns}</Return>
+            <Date>{item.createdAt}</Date>
           </TacticCard>
           </Space>
           ))}
