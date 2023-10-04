db38e8ecdb98484e822cf4f5c1cf8a2cf4e9cc0d
hajin618 <1998rlagkwls@gmail.com>
2023-10-04 19:46:32 +0900

    feat: 자유게시글 좋아요 순 정렬 로직 추가

--- backend/board/src/main/java/com/olock/blockstotck/board/domain/freeboard/persistence/entity/FreePost.java
+++ backend/board/src/main/java/com/olock/blockstotck/board/domain/freeboard/persistence/entity/FreePost.java
@@ -6,6 +6,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Formula;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,6 +22,10 @@ public class FreePost extends BaseEntity {
     private String title;
     private String content;
     private long hit;
+    @OneToMany(mappedBy = "freePost", cascade = CascadeType.ALL)
+    private List<FreePostLike> freePostLikes = new ArrayList<>();
+    @Formula("(select count(*) from free_post_like where free_post_like.free_post_id=id)")
+    private long likes;
 
     public FreePost(long memberId, String title, String content, long hit){
         super();
