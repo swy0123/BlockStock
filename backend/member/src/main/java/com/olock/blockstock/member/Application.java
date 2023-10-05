cc017622c1eab21dc9bd2b9a366442050b6c66e6
210-reverof <sheltonwon@gmail.com>
2023-10-05 19:15:49 +0900

    fix: 타임존 수정

--- backend/member/src/main/java/com/olock/blockstock/member/Application.java
+++ backend/member/src/main/java/com/olock/blockstock/member/Application.java
@@ -3,6 +3,7 @@ package com.olock.blockstock.member;
 import com.olock.blockstock.member.domain.member.persistence.MemberRepository;
 import com.olock.blockstock.member.domain.member.persistence.entity.Member;
 import com.olock.blockstock.member.domain.member.persistence.entity.Role;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +14,16 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.TimeZone;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.olock.blockstock.member")
 public class Application {
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
